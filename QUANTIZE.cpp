cc28c1d0ceb821bd641cc13e0d9186f6b35ff4c2
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2019-12-24 00:03:42 +0900

    Working on debugging

--- QUANTIZE.cpp
+++ QUANTIZE.cpp
@@ -5,30 +5,30 @@
 using namespace std;
 
 const int INF = 987654321;
-int original_array[100];
+int original_array[101];
 
-int partial_sum[100];
-int partial_square_sum[100];
+int partial_sums[101];
+int partial_square_sum[101];
 
-int cache[100][10];
+int cache[101][11];
 
 int len_original_array;
 int num_chunk;
 
 void FillPartialSum() {
     sort(original_array, original_array + len_original_array);
-    partial_sum[0] = original_array[0];
+    partial_sums[0] = original_array[0];
     partial_square_sum[0] = original_array[0] * original_array[0];
     for (int i = 0; i < len_original_array; i++) {
-        partial_sum[i] = partial_sum[i - 1] + original_array[i];
+        partial_sums[i] = partial_sums[i - 1] + original_array[i];
         partial_square_sum[i] = partial_square_sum[i - 1] + original_array[i] * original_array[i];
     }
 }
 
 int MinError(int index_from, int index_to) {
-    int sum = partial_sum[index_to] - (index_from == 0 ? 0 : partial_sum[index_from - 1]);
+    int sum = partial_sums[index_to] - (index_from == 0 ? 0 : partial_sums[index_from - 1]);
     int square_sum = partial_square_sum[index_to] - (index_from == 0 ? 0 : partial_square_sum[index_from - 1]);
-    int average = int(double(sum / (index_to - index_from + 1)) + 0.5);
+    int average = int(0.5 + ((double)sum / (index_to - index_from + 1)));
     int ret = square_sum - 2 * average * sum + average * average * (index_to - index_from + 1);
 }
 
@@ -54,6 +54,8 @@ int Quantize(int index_quantize_from, int num_leftchunk) {
     }
     return ret;
 }
+
+
 
 int main()
 {
