03d2e9f6b0ddd50eda119dbfbb1fb4bdc98a555d
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2020-01-12 17:34:57 +0900

    Update ARCTIC.cpp

--- ARCTIC.cpp
+++ ARCTIC.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include <math.h>
@@ -19,10 +12,10 @@ double x_coordinates[100], y_coordinates[100];
 double base_map[100][100];
 
 
-void PreProcess() { // fill the base map;
+void PreProcess() { // fill the base map
     for (int from = 0; from < 100; from++) {
         for (int to = 0; to < 100 ; to++) {
-            base_map[from][to] = sqrt(pow(y_coordinates[to] - y_coordinates[from], 2) + pow(x_coordinates[to] - x_coordinates[from], 2);
+            base_map[from][to] = sqrt(pow(y_coordinates[to] - y_coordinates[from], 2) + pow(x_coordinates[to] - x_coordinates[from], 2));
         }
     }
 }
@@ -32,12 +25,54 @@ bool PossibleToCommunicate(double radio_range) {
     visited[0] = true;
     queue<int> bfs_queue;
     bfs_queue.push(0);
+    int num_visited = 1;
+    
+    while(!bfs_queue.empty()) { // BFS search 
+        int from = bfs_queue.front();
+        bfs_queue.pop();
+        
+        for (int to = 0; to < num_base; to++) {
+            if ((base_map[from][to] <= radio_range) && !visited[to]) { // when base to go is closer than radio_range and not visited
+                bfs_queue.push(to);
+                num_visited++;
+            }
+        }
+    }
+    
+    return num_visited == num_base;
+    
+}
+
+double BinarySearch() {
+    double low = 0;
+    double high = 1001;
+    double mid = (low + high) / 2.0;
+    for (int i = 0; i < 100; i++) {
+        if (PossibleToCommunicate(mid)) {
+            high = mid;
+        }
+        else{
+            low = mid;
+        }
+    }
+    
 }
 int main()
 {
     int num_testcase;
+    cout.precision(3);
+    cout << fixed;
     
-    cout<<"Hello World";
+    cin >> num_testcase;
+    for (int i = 0; i < num_testcase; i++) {
+        cin >> num_base;
+        for (int j = 0; j < num_base; j++) {
+            cin >> x_coordinates[j];
+            cin >> y_coordinates[j];
+        }
+    }
+    
+    cout << BinarySearch() << endl;
 
     return 0;
 }
