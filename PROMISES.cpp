4127f43ba39945a7c0dd7e9b989672bbff66ce9d
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2020-03-08 23:46:30 +0900

    Update PROMISES.cpp

--- PROMISES.cpp
+++ PROMISES.cpp
@@ -7,6 +7,8 @@ int num_city;
 int num_current_road;
 int num_future_road;
 int graph[200][200];
+int useless = 0;
+const int INF = 987654321;
 
 void floyd() {
     for (int i = 0; i < num_city; i++) {
@@ -21,12 +23,16 @@ void floyd() {
     }
 }
 
-void update(int a, int b, int weight) {
+bool update(int a, int b, int weight) {
+    if (graph[a][b] <= weight) {
+        return false;
+    }
     for (int from = 0; from < num_city; from++) {
         for (int to = 0; to < num_city; to++) {
             graph[from][to] = min(graph[from][to], min(graph[from][a] + weight + graph[b][to], graph[from][b] + weight + graph[a][to] ));
         }
     }
+    return true;
 }
 
 int main()
@@ -40,7 +46,12 @@ int main()
         int from;
         int to;
         int weight;
-        int useless = 0;
+        useless = 0;
+        for (int x = 0; x < num_city; x++) {
+             for (int y = 0; y < num_city; y++) {
+                 graph[y][x] = INF;
+             }
+        }
         for (int j = 0; j < num_current_road; j++) {
             cin >> from;
             cin >> to;
@@ -53,10 +64,9 @@ int main()
             cin >> from;
             cin >> to;
             cin >> weight;
-            if (graph[from][to] < weight) {
+            if (!update(from, to , weight)){
                 useless++;
             }
-            update(from, to, weight);
         }
         
         cout << useless << endl;
